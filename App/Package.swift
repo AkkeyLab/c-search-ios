14e379553bc38cbd67f4553cab95978e0c25df98
AKIO <17640999+AkkeyLab@users.noreply.github.com>
2022-12-30 00:31:44 +0900

    :recycle: Add note comment

--- App/Package.swift
+++ App/Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.7
 
+// https://github.com/pointfreeco/isowords/blob/main/App/Package.swift
 // Leave blank. This is only here so that Xcode doesn't display it.
 
 import PackageDescription
