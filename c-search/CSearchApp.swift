f8c6b74bb9a921ddbf98ae365c76e2efccb20fbe
AKIO <17640999+AkkeyLab@users.noreply.github.com>
2022-12-29 18:01:16 +0900

    :sparkles: Create base project

---
+++ c-search/CSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSearchApp.swift
+//  c-search
+//
+//  Created by AkkeyLab on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct CSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ c-search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  c-search
+//
+//  Created by AkkeyLab on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentViewPreviews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
